baa5b18a2bf41ecd4d7c3705eb8aa668a70e89f0
Dmitry.Kononets <d.cononec@gmail.com>
2024-02-02 12:26:55 +0300

    commit

--- watches/src/components/CifralWatch.tsx
+++ watches/src/components/CifralWatch.tsx
@@ -1,9 +1,18 @@
+import moment from 'moment';
+
 const CifralWatch = () => {
   const date = new Date();
   const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+  const showTime = () => {
+    const time = () => {
+      return time;
+    }
+    let intervalId = setInterval(() => time, 1000)
+  }
   return(
     <div>
-      <div>{}</div>
+      <div>{showTime()}</div>
     </div>
   )
 }
---
+++ watches_class/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    
+  }
+}
+// function App() {
+//   return (
+//     <>
+//     </>
+//   )
+// }
+
+export default App
