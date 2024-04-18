2c250da85f5a5f6b8a4805e7904ba765d097c55f
Dmitry.Kononets <d.cononec@gmail.com>
2024-04-19 01:59:48 +0300

    commit

--- CRUD/src/components/TODOList.jsx
+++ CRUD/src/components/TODOList.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import TODO from "./TODO";
 
-const testUrl = 'http://localhost:3000/TODOS'
+const testUrl = 'http://localhost:7070/notes'
 
 class TODOList extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       posts: [],
+      error: null,
+      isLoaded: false,
       text: '',
     }
   }
@@ -24,21 +26,29 @@ class TODOList extends React.Component {
       body: JSON.stringify(body),
       headers: headers
     }).then(response => {
-      return response.json();
+      if (response.ok) {
+        return response.json();
+      }
+      return response.json().then(error => {
+        const e = new Error("Что то не так: " + error);
+        e.data = error;
+        throw e
+      })
     })
   }
 
   componentDidMount() {
-    // this.sendRequest('GET', testUrl)
-    // .then(data => console.log(data))
-    // .catch(err => console.log(err))
     this.sendRequest('GET', testUrl)
       .then(data => this.setState({
-        posts: data
+        posts: data,
+        isLoaded: true,
       }))
-      .catch(err => console.log(err))
+      .catch(error => this.setState({
+        isLoaded: true,
+        error
+      }))
   }
-
+  
   addFunc = (e) => {
     e.preventDefault();
     this.sendRequest('POST', testUrl, {text: this.state.text})
@@ -47,7 +57,7 @@ class TODOList extends React.Component {
     
     this.sendRequest('GET', testUrl)
     .then(data => this.setState({
-      posts: data
+      posts: data,
     }))
     .catch(err => console.log(err))
   }
@@ -57,34 +67,37 @@ class TODOList extends React.Component {
     fetch(`${testUrl}/${e.target.id}`, {
       method: 'delete',
     })
-
+    
     this.sendRequest('GET', testUrl)
-    .then(data => this.setState({
-      posts: data
-    }))
-    .catch(err => console.log(err))
+      .then(data => this.setState({
+        posts: data
+      }))
+      .catch(err => console.log(err))
   }
 
   textareaOnCange = (e) => {
     console.log(this.state.text)
-    const { target } = e;
     this.setState({
-      text: target.value,
+      text: e.target.value,
     })
   }
 
   render() {
-    return (
+    const { isLoaded, error, posts, text } = this.state;
+    if (error) {
+      return <div>ОШИБКА</div>
+    } else if (!isLoaded) {
+      return <div>Загрузка...</div>
+    } else return (
       <div className="main-container">
         <div className="user-container">
-          {this.state.posts.map((post, i) => <TODO key={i} delOnClick={this.deleteFunc} post={post}/>)}
+          {posts.map((post) => <TODO key={post.id} delOnClick={this.deleteFunc} post={post}/>)}
         </div>
         <form>
-          <textarea value={this.state.text} onChange={this.textareaOnCange} name="" id="" cols="30" rows="10"></textarea>
+          <textarea value={text} onChange={this.textareaOnCange} name="" id="" cols="30" rows="10"></textarea>
           <button onClick={this.addFunc} className="btn add-btn"></button>
         </form>
       </div>
-
     )
   }
 }
